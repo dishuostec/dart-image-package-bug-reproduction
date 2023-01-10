c6fc59f2665ee556b0008bf9ad9bfa35bb16a440
dishuostec <dishuostec@gmail.com>
2023-01-10 09:48:52 +0800

    test

---
+++ v3/bin/v3.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:image/image.dart';
+
+const version = 'v3';
+void main(List<String> arguments) {
+  print(version);
+
+  File input = File('../test.png');
+
+  final image = decodeImage(input.readAsBytesSync());
+  if (image == null) {
+    print('$input could not be loaded.');
+    exit(1);
+  }
+
+  for (var interpolation in Interpolation.values) {
+    String suffix = interpolation.name;
+    final newFile = copyResize(
+      image,
+      width: image.width ~/ 2,
+      height: image.height ~/ 2,
+      interpolation: interpolation,
+    );
+
+    final output = File('../output-$suffix-$version.png');
+    output.createSync(recursive: true);
+    output.writeAsBytesSync(encodePng(newFile));
+  }
+}
